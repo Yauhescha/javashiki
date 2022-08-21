07a1d1b7cf72956c1a20b94f0fc1a171750c8a9e
Yauhescha <evgescha.maslov007@yandex.ru>
2022-08-21 21:28:13 +0300

    clear pom and add example

--- src/main/java/com/yauhescha/javashiki/util/ainer.java
+++ src/main/java/com/yauhescha/javashiki/util/ainer.java
@@ -1,25 +1,27 @@
 package com.yauhescha.javashiki.util;
 
-import com.yauhescha.javashiki.model.auth.AccessToken;
 import com.yauhescha.javashiki.model.domen.AnimeFull;
+import com.yauhescha.javashiki.service.AnimeApiService;
 
+import java.util.Optional;
+
+//TODO remove comment
+//String defaultLogin = "fpo81406";
 public class ainer {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         AuthShikimori authShikimori = new AuthShikimori();
-        AccessToken accessToken = authShikimori.initialAccessToken();
-
-
-        for(int i=1; i<200; i++){
-
-            AnimeFull animeDetail = authShikimori.getAnimeApi().getAnimeDetail(i);
-
-            System.out.println("________________________________________________");
-            System.out.println("anime with id: " + i);
-            System.out.println(animeDetail);
-            System.out.println();
-            System.out.println();
-            System.out.println();
-            System.out.println("________________________________________________");
+        AnimeApiService animeApi = authShikimori.getAnimeApi();
+        for (int i = 1; i < 10; i++) {
+            Optional<AnimeFull> maybeAnime = animeApi.getAnimeDetail(i);
+            if(maybeAnime.isPresent()) {
+                AnimeFull anime = maybeAnime.get();
+                System.out.println("________________________________________________");
+                System.out.println("anime with id: " + i);
+                System.out.println(anime);
+                System.out.println("________________________________________________");
+                System.out.println();
+                System.out.println();
+            }
         }
 
     }
