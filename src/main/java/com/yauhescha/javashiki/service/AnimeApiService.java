dfc162176ce9b7d434c98cf9791fbdf59ff74d5f
Yauhescha <evgescha.maslov007@yandex.ru>
2022-08-22 00:11:20 +0300

    implement new getRoles method

--- src/main/java/com/yauhescha/javashiki/service/AnimeApiService.java
+++ src/main/java/com/yauhescha/javashiki/service/AnimeApiService.java
@@ -1,15 +1,20 @@
 package com.yauhescha.javashiki.service;
 
 import com.yauhescha.javashiki.model.domen.AnimeFull;
+import com.yauhescha.javashiki.model.domen.Roles;
 import com.yauhescha.javashiki.model.domen.Screenshot;
 import com.yauhescha.javashiki.util.AuthShikimori;
 import lombok.RequiredArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static com.yauhescha.javashiki.constant.ShikiInfo.METHOD_ANIMES_GET_ID;
+import static com.yauhescha.javashiki.constant.ShikiInfo.METHOD_ANIMES_ROLES;
 import static com.yauhescha.javashiki.constant.ShikiInfo.METHOD_ANIMES_SCREENSHOTS;
 
 //TODO
@@ -31,16 +36,27 @@ public class AnimeApiService {
         String url = String.format(METHOD_ANIMES_SCREENSHOTS, animeId);
         Screenshot[] screenshots = new ApiRequest<>(auth, Screenshot[].class).execute(url);
         if (screenshots == null) {
-            throw new RuntimeException("Screenshots not found for anime with id: " + animeId);
+            return new ArrayList<>();
         }
         return Arrays.asList(screenshots);
     }
 
+    public List<Roles> getRoles(int animeId, boolean onlyCharacters) {
+        String url = String.format(METHOD_ANIMES_ROLES, animeId);
+        Roles[] roles = new ApiRequest<>(auth, Roles[].class).execute(url);
+        if (roles == null) {
+            return new ArrayList<>();
+        }
+        if (!onlyCharacters) {
+            return Arrays.asList(roles);
+        }
+        return Stream.of(roles)
+                .filter(role -> !role.getRoles().isEmpty())
+                .filter(role -> role.getRoles().contains("Main") || role.getRoles().contains("Supporting"))
+                .collect(Collectors.toList());
+    }
+
 //    public List<Anime> getAnimes() {
-//        return null;
-//    }
-//
-//    public List<Roles> getRoles(int animeId) {
 //        return null;
 //    }
 //
